7b0edd20abdc2bc29fdf9da81257bb81b2409c19
nick_krlv <fieruo@gmail.com>
2024-02-06 17:19:16 +0300

    finished

--- users/models.py
+++ users/models.py
@@ -8,8 +8,8 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
     email_token = models.CharField(max_length=50, **NULLABLE, verbose_name='email_token')
-    clients = models.ManyToManyField('mailing.Client',**NULLABLE, verbose_name='Клиенты')
-    mails = models.ManyToManyField('mailing.MailingService', **NULLABLE, related_name='users', verbose_name='Рассылки')
+    clients = models.ManyToManyField('mailing.Client', verbose_name='Клиенты')
+    mails = models.ManyToManyField('mailing.MailingService', related_name='users', verbose_name='Рассылки')
     is_active = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
